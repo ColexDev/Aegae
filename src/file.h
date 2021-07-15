8e8fdff2e6fc90678788fba937a70781a5ca528a
Cole <cole@colex.dev>
2021-07-15 16:57:39 -0400

    Delete src directory

--- src/file.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-#include <iostream>
-void eraseFileLine(const std::string par_PATH, const std::string par_ERASELINE);
-void write_database_to_vector();
--- src/table.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <iostream>
-
-void find_longest();
-std::string add_space_after(std::string &spaceString, int numSpaces);
-int find_number_of_spaces(std::string current, int longest);
-void fill_all_entries_no_spaces();
-void draw_table();
