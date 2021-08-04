44badb7b43007fdaedd259c6bf7e3d178e60bd32
ColexDev <cole@colex.dev>
2021-08-04 16:26:16 -0400

    Updated keybinds

--- def.h
+++ def.h
@@ -1,14 +1,14 @@
 constexpr int HORIZONTAL = 0;
 constexpr int VERTICAL = 1;
 
-constexpr int KEY_ESCAPE = 27;
 constexpr int KEY_BACKSLASH = 47;
+constexpr int KEY_A = 97;
 constexpr int KEY_B = 98;
-constexpr int KEY_F = 102;
 constexpr int KEY_H = 104;
 constexpr int KEY_J = 106;
 constexpr int KEY_K = 107;
 constexpr int KEY_L = 108;
+constexpr int KEY_M = 109;
 constexpr int KEY_N = 110;
 constexpr int KEY_Q = 113;
 constexpr int KEY_X = 120;
